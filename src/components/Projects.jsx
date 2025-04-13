e7dd3352023d0f21cf68edb3bae9326b16dc5b43
Tanushree Mohanty <mohantytanushree234@example.com>
2025-04-13 12:22:49 +0530

    project updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react";
 import { Container, Row, Col, Button, Card } from "react-bootstrap";
 import "./Projects.css";
-import p1 from "../assets/project1.png"; 
+import p1 from "../assets/quicklist.png";
 import p2 from "../assets/weathery.png";
 
 const projectData = [
-  // {
-  //   title: "TaskSync",
-  //   category: "Beginner Level",
-  //   description: "A sleek and minimalistic Todo application built with React.js that allows users to efficiently manage daily tasks with a clean and intuitive interface.",
-  //   image: p1,
-  //   demoLink: "https://task-sync-app.vercel.app/",
-  //   githubLink: "https://github.com/TanushreeMohanty/TaskSync.git",
-  //   techStack: ["React.js"],
-  // },
+  {
+    title: "QuickList",
+    category: "Beginner Level",
+    description: "A sleek and minimalistic Todo application built with React.js that allows users to efficiently manage daily tasks with a clean and intuitive interface.",
+    image: p1,
+    demoLink: "https://quick-list-iota.vercel.app/",
+    githubLink: "https://github.com/TanushreeMohanty/QuickList",
+    techStack: ["React.js"],
+  },
   {
     title: "Weathery",
     category: "Intermediate Level",
